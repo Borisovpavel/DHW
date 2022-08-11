dee49636bdd5ee04779e57880691ab0741e15a55
unknown <borisov86@inbox.ru>
2022-08-11 19:18:24 +0300

    TASK 8

---
+++ 4/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.WriteLine("Самое большое " + a );
+    }
+    else if (c > b)
+    {
+        Console.WriteLine("Самое большое " + c );
+    }
+    else
+    {
+        Console.WriteLine("Самое большое " + b );
+    }
+
+} else if (b > a)
+{
+    if (b > c)
+    {
+        Console.WriteLine("Самое большое " + b );
+    }
+    else
+    {
+        Console.WriteLine("Самое большое " + c );
+    }
+}
+
+
+
+
+    
+
