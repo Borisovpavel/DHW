17f99ccea3fde23873aa8df4d5d10e9afe05de9f
unknown <borisov86@inbox.ru>
2022-08-11 18:49:17 +0300

    TASK 2

---
+++ 1/Program.cs
@@ -0,0 +1,22 @@
+﻿{
+Console.WriteLine("Введите первое число:");
+// Считываем строку, написанное пользователем в консоль и переформатируем в int
+ int a = int.Parse( Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+ int b = int.Parse( Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+ int c = int.Parse( Console.ReadLine());
+ if (a > b && a > c)
+ {
+    Console.WriteLine("a max = "+ a);
+ }
+ else if (b > a && b > c)
+ {
+    Console.WriteLine("b max = "+ b);
+ }
+ else if(c > b && c > a)
+ {
+    Console.WriteLine("c max = "+ c);
+ }
+}
+ 
