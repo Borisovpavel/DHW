816012a4cfa9fa434650f380d524a4414ab2b7c0
unknown <borisov86@inbox.ru>
2022-08-11 18:56:18 +0300

    TASK 1

---
+++ 2/Program.cs
@@ -0,0 +1,20 @@
+﻿{
+Console.WriteLine("Введите первое число:");
+// Считываем строку, написанное пользователем в консоль и переформатируем в int
+ int a = int.Parse( Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+ int b = int.Parse( Console.ReadLine());
+if(a > b)
+{
+    Console.WriteLine("Первое числобольше второго и равно = "+ a);
+}
+else if (b > a)
+{
+    Console.WriteLine("Второе число больше первого и ровно = " + b);
+}
+else
+{
+    Console.WriteLine("Первое и второе числа равны : " + a);
+}
+}
+ 
