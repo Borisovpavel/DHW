51796f4b105132ba81e2b9c86ecf7113202538ad
unknown <borisov86@inbox.ru>
2022-08-14 22:23:06 +0300

    TASK 10

---
+++ 5/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter some 3-x number->> get second one part ");
+string x = Console.ReadLine();
+threelnOneOut(x);
+void threelnOneOut(string somelnt)
+{
+    if (somelnt.Length == 3)
+    {
+        Console.WriteLine(somelnt.Trim(somelnt[0],somelnt[2]));
+    }
+    else
+    {
+        Console.WriteLine("wrong data try again ");
+        somelnt = Console.ReadLine();
+        threelnOneOut(somelnt);
+    }
+}
