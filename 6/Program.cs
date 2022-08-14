d3750790aa22b625fc91b4bbe5ee10dd00a177c9
unknown <borisov86@inbox.ru>
2022-08-14 22:29:41 +0300

    TASK 13

---
+++ 6/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter 3-x number ");
+string y = Console.ReadLine();
+ThirdNumber(y);
+
+void ThirdNumber(string somelnt)
+{
+    if (somelnt.Length == 3)
+    {
+        Console.WriteLine(somelnt.Trim(somelnt[0],somelnt[1]));
+    }
+    else
+    {
+        Console.WriteLine("wrond data try again ");
+        somelnt = Console.ReadLine();
+        ThirdNumber(somelnt);
+    }
+}
