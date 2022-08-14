d71f7fbfbb63e76d1dc4df0e351f6b34d7e633a4
unknown <borisov86@inbox.ru>
2022-08-14 22:35:38 +0300

    TASK 15

---
+++ 7/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter some day of a week (1.2.3.4.5.6.7)");
+string dayOfAWeek = Console.ReadLine();
+if (dayOfAWeek.Equals("6") || dayOfAWeek == "7")
+{
+    Console.WriteLine("WeekEnd ");
+}
+else Console.WriteLine("nah");
